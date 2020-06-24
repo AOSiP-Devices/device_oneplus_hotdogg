3f5d907e9714f3c11c6c3748557e5a09fa8a2db6
Matt Evangelisti <mjevange@gmail.com>
2020-06-23 21:23:45 -0400

    hotdogg: Default to 5G

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,5 +1,6 @@
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.radio.multisim.config=ssss \
     persist.vendor.radio.5g_mode_pref=1 \
+    ro.telephony.default_network=33 \
     ro.vendor.boot.project_name=19861 \
     rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
