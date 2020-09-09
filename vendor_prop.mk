69b3ef17df47de397691502cbfb9fa8c7f33ac01
Matt Evangelisti <mjevange@gmail.com>
2020-09-09 19:09:51 -0400

    hotdogg: Fix fingerprint sensor
    libgf_ud_hal.so looks for vendor.boot.verifiedbootstate to be set to the phones true state
    in our case, for an unlocked phone, it must be orange
    (green = locked with factory rom, orange = unlocked no checking, yellow = locked with user key)

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -3,4 +3,5 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.vendor.radio.5g_mode_pref=1 \
     ro.telephony.default_network=33 \
     ro.vendor.boot.project_name=19861 \
-    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    vendor.boot.verifiedbootstate=orange
